bf5c9955ffb77a234b8f0e1df04ae58c87c49778
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-06-05 05:18:22 +0530

    defi app setup

---
+++ evm-advanced/defi-holders/backend/contracts/NFTMarketplace.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import ERC721 interface and SafeMath library
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract NFTMarketplace {
+    using SafeMath for uint256;
+
+    struct Bid {
+        address bidder;
+        uint256 amount;
+    }
+
+    mapping(address => mapping(uint256 => Bid)) public bids;
+
+    function placeBid(address nftAddress, uint256 tokenId) external payable {
+        require(msg.value > 0, "Bid amount must be greater than 0");
+
+        // Get the current highest bid for the NFT
+        Bid storage highestBid = bids[nftAddress][tokenId];
+
+        // Require new bid to be higher than the current highest bid
+        require(msg.value > highestBid.amount, "Bid amount must be higher than the current highest bid");
+
+        // Return the previous bid amount to the previous bidder
+        if (highestBid.amount > 0) {
+            address payable previousBidder = payable(highestBid.bidder);
+            previousBidder.transfer(highestBid.amount);
+        }
+
+        // Update the highest bid
+        highestBid.bidder = msg.sender;
+        highestBid.amount = msg.value;
+    }
+
+    function acceptBid(address nftAddress, uint256 tokenId) external {
+        // Get the highest bid for the NFT
+        Bid storage highestBid = bids[nftAddress][tokenId];
+
+        // Require the caller to be the owner of the NFT
+        require(IERC721(nftAddress).ownerOf(tokenId) == msg.sender, "Only the owner can accept a bid");
+
+        // Transfer the NFT to the bidder
+        IERC721(nftAddress).transferFrom(msg.sender, highestBid.bidder, tokenId);
+
+        // Transfer the bid amount to the seller
+        address payable seller = payable(msg.sender);
+        seller.transfer(highestBid.amount);
+
+        // Clear the bid data
+        delete bids[nftAddress][tokenId];
+    }
+}
